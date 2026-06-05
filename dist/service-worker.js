af02eb009119b056c0cab9cd9b1157bf97c2c01d
humansinstitute <petewinn@gmail.com>
2026-06-05 15:21:50 +0800

    Refresh PH1-03 build assets

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260605-0720-15-961";
+const BUILD_ID = "20260605-0720-16-962";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
