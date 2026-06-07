827cadc230b04be125a3d7c4c98597c7671bbdd6
humansinstitute <petewinn@gmail.com>
2026-06-07 08:55:10 +0800

    Update Flight Deck build artifacts

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260607-0048-8-1012";
+const BUILD_ID = "20260607-0054-12-1016";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
