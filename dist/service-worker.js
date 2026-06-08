f77523b998c3f0ddc964531201ed190a7b278a5a
humansinstitute <petewinn@gmail.com>
2026-06-08 20:12:07 +0800

    Refresh Flight Deck build after revocation verification

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260608-0800-6-1029";
+const BUILD_ID = "20260608-1211-7-1030";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
