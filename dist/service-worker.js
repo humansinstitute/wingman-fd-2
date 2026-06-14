fa4c9b5005dd80c7f2c37f3d847e5a1627a3ebb7
humansinstitute <petewinn@gmail.com>
2026-06-14 15:44:27 +0800

    Refine scope picker channel row styling

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260614-0715-5-1137";
+const BUILD_ID = "20260614-0743-6-1138";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
