1d33e36fb1873d5165c3943cc6de37dff1114868
humansinstitute <petewinn@gmail.com>
2026-06-14 15:46:39 +0800

    Tighten channel scope picker styling

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260614-0743-6-1138";
+const BUILD_ID = "20260614-0745-7-1139";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
