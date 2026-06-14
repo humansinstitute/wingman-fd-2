aa2627db30cea5f6e5682769385d65ad3cf0d707
humansinstitute <petewinn@gmail.com>
2026-06-14 15:17:00 +0800

    Move scope picker into channel row

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260614-0026-4-1136";
+const BUILD_ID = "20260614-0715-5-1137";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
