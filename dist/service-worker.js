990c8feb8cc137f8c3d82ff139219e47af3aacc5
humansinstitute <petewinn@gmail.com>
2026-06-17 12:14:35 +0800

    Use ghost icons for Daily Note pager

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260617-0411-29-1222";
+const BUILD_ID = "20260617-0414-30-1223";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
