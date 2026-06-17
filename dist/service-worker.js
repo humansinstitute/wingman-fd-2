32cbc55671b84b9050b007e7606b0d5d3e5e8c4c
humansinstitute <petewinn@gmail.com>
2026-06-17 13:05:35 +0800

    Fix Daily Note mobile overflow

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260617-0414-30-1223";
+const BUILD_ID = "20260617-0505-31-1224";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
