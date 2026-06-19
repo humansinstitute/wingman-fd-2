01ac53c69854a6079133ea1209064645fc339c28
humansinstitute <petewinn@gmail.com>
2026-06-19 21:09:15 +0800

    Add Docs section header

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260619-1153-28-1277";
+const BUILD_ID = "20260619-1308-29-1278";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
