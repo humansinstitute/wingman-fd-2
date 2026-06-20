3606ac29d49322737cf33f07916f907c37b84727
humansinstitute <petewinn@gmail.com>
2026-06-20 16:00:29 +0800

    Rebuild Flight Deck production assets

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260620-0711-14-1295";
+const BUILD_ID = "20260620-0800-15-1296";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
