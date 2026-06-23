755eda5862acb32909cf2a7253023b14cc669d47
humansinstitute <petewinn@gmail.com>
2026-06-23 12:13:22 +0800

    Rebuild Flight Deck dist after Tiptap fix

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260623-0410-18-1345";
+const BUILD_ID = "20260623-0412-19-1346";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
