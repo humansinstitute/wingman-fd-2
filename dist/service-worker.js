fa5d1c0bb7b201fde550373a1c3fc59b010c2ff5
humansinstitute <petewinn@gmail.com>
2026-06-23 22:53:03 +0800

    Polish task field textarea styling

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260623-1434-45-1372";
+const BUILD_ID = "20260623-1442-46-1373";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
