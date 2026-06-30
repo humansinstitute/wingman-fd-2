37d68d53d21f59f2fa70c32f9e14f5929ee6313d
humansinstitute <petewinn@gmail.com>
2026-06-30 21:23:43 +0800

    Align file actions in Files toolbar

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260630-1311-3-1411";
+const BUILD_ID = "20260630-1318-4-1412";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
