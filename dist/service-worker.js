a73db39218a6a64476fb68a989467b5514187a18
Pete Winn <petewinn@gmail.com>
2026-07-16 14:00:02 +0800

    Refresh workroom creation UI build output

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260716-0559-13-1440";
+const BUILD_ID = "20260716-0559-14-1441";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
