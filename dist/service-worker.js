b2b7890bb294b4daa8d5e56236b4222f98f8071c
Pete Winn <petewinn@gmail.com>
2026-07-16 15:06:47 +0800

    Refresh workroom visible people build output

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260716-0704-21-1448";
+const BUILD_ID = "20260716-0706-22-1449";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
