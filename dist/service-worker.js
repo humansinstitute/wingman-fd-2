938a8ef31631e73adbdd83ef4fd3e64483cf9571
Pete Winn <petewinn@gmail.com>
2026-07-16 14:31:25 +0800

    Align mobile composer with action buttons

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260716-0629-18-1445";
+const BUILD_ID = "20260716-0631-19-1446";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
