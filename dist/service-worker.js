7e37b6a989da7953057150eb603c3ba528d34435
Pete Winn <petewinn@gmail.com>
2026-07-24 13:52:09 +0800

    Build Flight Deck version 1516

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260724-0537-9-1515";
+const BUILD_ID = "20260724-0551-10-1516";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
